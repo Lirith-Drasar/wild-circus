f681c0f37d8420e28ed2a25ff6ce21d47c7cfa8a
Lirith-Drasar <ma.rabouin@laposte.net>
2020-01-31 09:39:39 +0100

    image, footer and fixture new css

--- src/DataFixtures/ShowsFixture.php
+++ src/DataFixtures/ShowsFixture.php
@@ -11,11 +11,32 @@ class ShowsFixture extends Fixture
     public function load(ObjectManager $manager)
     {
         $show = new Shows();
-        $show->setName('WinterShows');
-        $show->setImage('index.jpeg');
+        $show->setName('WinterShow');
+        $show->setImage('winter.jpeg');
         $show->setDescription('je suis une description');
 
         $manager->persist($show);
+
+        $show1 = new Shows();
+        $show1->setName('SummerShow');
+        $show1->setImage('Summer.jpeg');
+        $show1->setDescription('je suis une description');
+
+        $manager->persist($show1);
+
+        $show2 = new Shows();
+        $show2->setName('AutomnShow');
+        $show2->setImage('automn.jpeg');
+        $show2->setDescription('je suis une description');
+
+        $manager->persist($show2);
+
+        $show3 = new Shows();
+        $show3->setName('SpringShow');
+        $show3->setImage('spring.jpeg');
+        $show3->setDescription('je suis une description');
+
+        $manager->persist($show3);
 
         $manager->flush();
     }
